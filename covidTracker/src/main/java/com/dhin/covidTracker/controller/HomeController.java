ad3535ed2948b2805f876384d49c7198cb09421c
DhineshMurali <70338233+DhineshMurali@users.noreply.github.com>
2021-06-15 20:46:14 +0530

    Add files via upload

---
+++ covidTracker/src/main/java/com/dhin/covidTracker/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.dhin.covidTracker.controller;
+
+import com.dhin.covidTracker.modules.LocationStats;
+import com.dhin.covidTracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
